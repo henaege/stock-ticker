b797bdf5f0bcd6003f89bb4e85d70d8d5f589c4b
Drew Tolliver <henaege@gmail.com>
2017-05-22 11:30:48 -0400

    added submit functionality

--- yahoo-stock-ticker/js/scripts.js
+++ yahoo-stock-ticker/js/scripts.js
@@ -12,7 +12,12 @@ $(document).ready(()=> {
         // Get whatever the user typed and stash it in a var
         let symbol = $('#symbol').val();
 
-        // http://query.yahooapis.com/v1/public/yql?q=env%20%27store://datatables.org/alltableswithkeys%27;select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22goog%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json
+        let url = `http://query.yahooapis.com/v1/public/yql?q=env%20%27store://datatables.org/alltableswithkeys%27;select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22${symbol}%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json`
+
+        console.log(url)
+        $.getJSON(url, (theDataJSFound)=> {
+            console.log(theDataJSFound);
+        });
         
     });
 
